8f93be793e105c0132b42b64f52b871f60a96a08
Ankit Singh <ankitsingh.r@prodapt.com>
2025-08-30 11:43:25 +0530

    Added customer service CRUD Operations and role change

--- src/service/CustomerService.java
+++ src/service/CustomerService.java
@@ -1,4 +1,18 @@
 package service;
 
+import model.Customer;
+
+import java.util.List;
+import java.util.UUID;
+
 public interface CustomerService {
+
+    Customer addCustomer(Customer customer);
+    Customer getCustomer(UUID id);
+    void deleteCustomer(UUID id);
+    Customer updateCustomer(Customer customer);
+    List<Customer> getAllCustomer();
+
+    boolean roleChange(UUID actingAdminId,UUID id,String newRole);
+
 }
--- src/ui/TelecomApp.java
+++ src/ui/TelecomApp.java
@@ -1,22 +1,34 @@
 package ui;
 
+import exceptions.InvalidCustomerException;
+import exceptions.UnauthorizedAccessException;
 import model.Customer;
 import model.Plan;
 import model.Subscription;
 import repo.CustomerRepo;
 import repo.PlanRepo;
 import repo.SubscriptionRepo;
+import service.CustomerService;
+import service.CustomerServiceImpl;
 import service.SubscriptionService;
 import service.SubscriptionServiceImpl;
+
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Scanner;
 import java.util.UUID;
 
 public class TelecomApp {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
         CustomerRepo customerRepo = new CustomerRepo();
         PlanRepo planRepo = new PlanRepo();
         SubscriptionRepo subscriptionRepo = new SubscriptionRepo(customerRepo, planRepo);
         SubscriptionService subscriptionService = new SubscriptionServiceImpl(customerRepo, planRepo, subscriptionRepo);
+        CustomerService customerService = new CustomerServiceImpl(customerRepo);
+
 
         List<Customer> admins = customerRepo.findAdmins();
         String adminRole = admins.get(0).getRole(); // "admin"
@@ -39,5 +51,95 @@ public class TelecomApp {
         } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
         }
+
+//        boolean flag=true;
+//        while (flag){
+//            System.out.println("===== TELECOM APP MENU =====");
+//            System.out.println("1. Add New Customer");
+//            System.out.println("2. View All Customers");
+//            System.out.println("3. get Customer By Id");
+//            System.out.println("4. Delete Customer (Admin only)");
+//            System.out.println("5. role change (Admin Only)");
+//
+//            int choice=Integer.parseInt(sc.nextLine());
+//
+//            switch (choice){
+//                case 1:
+//                    System.out.print("Enter Name: ");
+//                    String name = sc.nextLine();
+//                    System.out.print("Enter Email: ");
+//                    String email = sc.nextLine();
+//                    System.out.print("Enter Password: ");
+//                    String password = sc.nextLine();
+//
+//                    Customer newCustomer = new Customer(
+//                            null,
+//                            name,
+//                            email,
+//                            password,
+//                            null,      // referredBy
+//                            false,     // creditBlocked
+//                            LocalDate.now(),
+//                            "customer" // default role
+//                    );
+//                    customerService.addCustomer(newCustomer);
+//                    System.out.println("Customer added: " + newCustomer);
+//                    break;
+//                case 2:
+//                    System.out.println("--- Customer List ---");
+//                    customerService.getAllCustomer().forEach(System.out::println);
+//                    break;
+//                case 3:
+//                    System.out.print("Enter Customer ID: ");
+//                    UUID getId = UUID.fromString(sc.nextLine());
+//
+//                    Customer found = customerService.getCustomer(getId);
+//                    if (found != null) {
+//                        System.out.println("Found Customer: " + found);
+//                    } else {
+//                        throw new InvalidCustomerException("Customer not found.");
+//                    }
+//                    break;
+//                case 4:
+//                    System.out.println("--- Delete Customer ---");
+//                    System.out.print("Enter Admin Customer ID: ");
+//                    UUID adminId = UUID.fromString(sc.nextLine());
+//
+//                    System.out.print("Enter Target Customer ID to delete: ");
+//                    UUID deleteTargetId = UUID.fromString(sc.nextLine());
+//
+//
+//                    Customer acting = customerRepo.findById(adminId);
+//                    if (acting != null && "admin".equalsIgnoreCase(acting.getRole())) {
+//                        customerService.deleteCustomer(deleteTargetId);
+//                        System.out.println("Customer deleted successfully.");
+//                    } else {
+//                        throw new UnauthorizedAccessException("Access Denied");
+//                    }
+//
+//                    break;
+//                case 5:
+//                    System.out.println("--- Change Role ---");
+//                    System.out.print("Enter Acting Admin ID: ");
+//                    UUID roleAdminId = UUID.fromString(sc.nextLine());
+//                    System.out.print("Enter Target Customer ID: ");
+//                    UUID targetCustId = UUID.fromString(sc.nextLine());
+//                    System.out.print("Enter New Role (admin/customer): ");
+//                    String newRole = sc.nextLine();
+//
+//                    try {
+//                        boolean changed = customerService.roleChange(roleAdminId, targetCustId, newRole);
+//                        if (changed) {
+//                            System.out.println("Role updated successfully.");
+//                        } else {
+//                            System.out.println("Target customer not found.");
+//                        }
+//                    } catch (Exception e) {
+//                        System.out.println("Error: " + e.getMessage());
+//                    }
+//                    break;
+//
+//            }
+       // }
     }
 }
