3ee56afa66fd2b6a5b594222688378612cf521e4
Sahana12345678 <112747329+Sahana12345678@users.noreply.github.com>
2025-08-31 18:06:13 +0530

    Updated AnalyticsService.java

--- src/service/AnalyticsService.java
+++ src/service/AnalyticsService.java
@@ -1,4 +1,21 @@
 package service;
 
+
+import java.util.DoubleSummaryStatistics;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import model.Customer
+import model.Subscription
+import model.Plan
+import model.Invoice
+import model.UsageRecord
+
 public interface AnalyticsService {
+    List<Map.Entry<UUID, Double>> topDataUsers(String role, List<Invoice> invoices, int n);
+    Map<Plan,Double> arpuByPlan(String role,List<Invoice> invoices);
+    Map<UUID, DoubleSummaryStatistics> getOverageStatsByPlan(String role,List<Invoice> invoices, List<Subscription> subscriptions);
+    List<String> detectCreditRisk(String role,List<Invoice> invoices,List<Subscriptions> subscriptions,List<Customer> customers);
+    List<String> planRecommendation(String role,)
 }
+
